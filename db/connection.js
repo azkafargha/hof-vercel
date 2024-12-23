4a84645ad49914df8e3cc553259a6adba34a7bc5
azkafargha <azkafarghaa@gmail.com>
2024-12-23 18:49:59 +0700

    nambah dotenv

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
-const { Pool } = require("pg");
-
+const Pool = require("pg").Pool;
 const pool = new Pool({
   connectionString: process.env.DB_URL,
+  connectionString: process.env.TOKEN_SECRET,
 });
 
 module.exports = pool;
