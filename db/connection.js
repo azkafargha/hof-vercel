bcc8d6b9abf53fb29ed293235f27b97e57ecb2ed
azkafargha <azkafarghaa@gmail.com>
2024-12-23 18:19:23 +0700

    fix db

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  // local
-  user: "postgres",
-  host: "localhost",
-  database: "db_hof",
-  password: "admin",
-  port: 5432,
-
-  // deployed
-  //   connectionString: process.env.DB_URL,
+  connectionString: process.env.DB_URL,
 });
 
 module.exports = pool;
